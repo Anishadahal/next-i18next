d89198cd490dae634a427b4279617e2ef03f04cc
Anishadahal <anishadahalofficial@gmail.com>
2022-08-25 00:39:10 +0545

    addtrans component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useTranslation } from "next-i18next";
+import { Trans, useTranslation } from "next-i18next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   const {t} = useTranslation("common")
   return (
     <div className={styles.container}>
       <p>{t("Hello")}</p>
+      <p>This is a link to Google.</p>
+      <Trans i18nKey="userMessagesUnread">
+        {t("Hello <strong>there</strong>, you have 5 unread messages.<a href='#'>Read messages.</a>")}
+      </Trans>
     </div>
   );
 };
